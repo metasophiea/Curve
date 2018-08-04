edb3f4a0990d3ce30b954fa0bbf25d03e49dbdcf
metasophiea <metasophiea@gmail.com>
2018-08-04 14:47:26 +0100

    this damn update

--- testSituations/2 - elements test/main.js
+++ testSituations/2 - elements test/main.js
@@ -10,9 +10,5 @@
     testObject_1.io.externalData_1.connectTo(testObject_2.io.externalData_1);
 
 // auto position viewpoint
-<<<<<<< HEAD
     // __globals.utility.workspace.gotoPosition( -2292.56, -901.214, 3.91813, 0);
-=======
-    // __globals.utility.workspace.gotoPosition(-1533.02, -2161.7, 8.38412, 0);
->>>>>>> b0efefdb8881383e223b29fce1c59ba19d41116c
 //     console.log(__globals.utility.workspace.currentPosition());
